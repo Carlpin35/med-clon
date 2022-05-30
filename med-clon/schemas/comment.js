8c3a7662c062263a3e47f03a1e9fb695476d6739
Carlpin35 <carlpin800@gmail.com>
2022-05-30 11:04:33 -0300

    first try

---
+++ med-clon/schemas/comment.js
@@ -0,0 +1,38 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      name: 'approved',
+      title: 'Approved',
+      type: 'boolean',
+      description: "comments won't show on the site without approval",
+    },
+    {
+      name: 'email',
+      type: 'string',
+      },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: {type: 'author'},
+    },
+    {
+      name: 'comment',
+      type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'reference',
+      to: [{type: 'post'}],
+    },
+    
+  ],
+
+};
